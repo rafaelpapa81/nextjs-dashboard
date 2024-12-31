3906343755ab948d58d0c0850a0fe0f087bf0e2b
Rafael Papa <rafaelpapa81@gmail.com>
2024-12-31 09:45:09 -0500

    nextjs-dashboard load multiple components at the same time using skeleton

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -32,7 +32,7 @@ export async function fetchLatestInvoices() {
 
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-    console.log('Fetching revenue data...');
+    console.log('Fetching invoices data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<LatestInvoiceRaw>`
--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,7 +1,7 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
+//import { Revenue } from '@/app/lib/definitions';
 import { fetchRevenue } from '@/app/lib/data';
 
 // This component is representational only.
@@ -17,7 +17,7 @@ import { fetchRevenue } from '@/app/lib/data';
 // }) {
 export default async function RevenueChart() { // Make component async, remove the props
   const revenue = await fetchRevenue(); // Fetch data inside the component
-  
+
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
