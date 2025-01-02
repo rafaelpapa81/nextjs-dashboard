d2f1a12e6fb8144df93b408cec820278756d268c
Rafael Papa <rafaelpapa81@gmail.com>
2025-01-01 21:12:24 -0500

    nextjs-dashboard parse obj form using zod and create sql to insert into vercel

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,14 +1,43 @@
 'use server';
 
-export async function createInvoice(formData: FormData) {
-    // Tip: If you're working with forms that have many fields, you may want to consider using the entries() method with JavaScript's Object.fromEntries(). For example:
-    // const rawFormData = Object.fromEntries(formData.entries())
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
 
-    const rawFormData = {
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    };
-    // Test it out:
-    console.log(rawFormData);
+// coerce (change) the type of the amount field from string to number
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  // Tip: If you're working with forms that have many fields, you may want to consider using the entries() method with JavaScript's Object.fromEntries(). For example:
+  // const rawFormData = Object.fromEntries(formData.entries())
+
+//   const rawFormData = {
+//     customerId: formData.get('customerId'),
+//     amount: formData.get('amount'),
+//     status: formData.get('status'),
+//   };
+//   // Test it out:
+//   console.log(rawFormData);
+//   console.log(typeof rawFormData.amount);
+
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  
 }
