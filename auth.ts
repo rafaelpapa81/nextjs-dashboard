643f29088234c99fac2f271926884a7aec193d2a
Rafael Papa <rafaelpapa81@gmail.com>
2025-01-03 09:27:06 -0500

    nextjs-dashboard check if passwd match using bcrypt

--- auth.ts
+++ auth.ts
@@ -29,8 +29,12 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordMatch) return user;
         }
  
+        console.log('Invalid credentials');
         return null;
       },
     }),
