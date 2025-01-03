8fbc0e5b29dd392d7e78752ec48ba4963854c6bf
Rafael Papa <rafaelpapa81@gmail.com>
2025-01-03 09:17:38 -0500

    nextjs-dashboard validate the credentials

--- auth.ts
+++ auth.ts
@@ -1,11 +1,17 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
-
-// Although we're using the Credentials provider, it's generally recommended 
-//   to use alternative providers such as OAuth or email providers. See the NextAuth.js docs for a full list of options.
-
+import { z } from 'zod';
+ 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [Credentials({})],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+      },
+    }),
+  ],
 });
