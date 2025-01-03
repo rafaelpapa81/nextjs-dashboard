39e6142a87289a5165c4bce9fbfb1d2655f14cf4
Rafael Papa <rafaelpapa81@gmail.com>
2025-01-03 09:13:27 -0500

    nextjs-dashboard add the credentials provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,11 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
+import Credentials from 'next-auth/providers/credentials';
+
+// Although we're using the Credentials provider, it's generally recommended 
+//   to use alternative providers such as OAuth or email providers. See the NextAuth.js docs for a full list of options.
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  providers: [Credentials({})],
 });
